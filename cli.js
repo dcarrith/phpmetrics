470ffd3a9f80538800111aaef3dfe394b635b73e
Dave <dcarrith@gmail.com>
2016-01-26 23:09:36 -0500

    Committing the latest changes to cli.js

--- cli.js
+++ cli.js
@@ -3,7 +3,18 @@
 var exec = require('child_process').exec
 var args = process.argv.slice(2)
 var cli = require('./').cli
-var suppress = false
+var winston = require('winston');
+
+var logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.File)({filename: 'node.log'})
+    ]
+});
+
+if (process.env.NODE_ENV === 'test') {
+    console.log('Adding winston.transports.Console as the logger.');
+    logger.add(winston.transports.Console, {prettyPrint: true});
+}
 
 if (args.length === 0 || (args.length === 1 && args[0] === 'help')) {
   console.log([
@@ -14,16 +25,12 @@ if (args.length === 0 || (args.length === 1 && args[0] === 'help')) {
     '  phpmetrix config.yml',
     ''
   ].join('\n'))
+    process.exit()
 }
 
 if (args[0] === '-v' || args[0] === '--version') {
   var pkg = require('./package.json')
-  console.log(pkg.version)
   process.exit()
-}
-
-if (args.indexOf('-s') !== -1 || args.indexOf('--suppress') !== -1) {
-  suppress = false
 }
 
 exec('php -v', function (err, stdout, stderr) {
@@ -34,4 +41,4 @@ exec('phpmetrics --version', function (err, stdout, stderr) {
   if (err) throw new Error(err)
 })
 
-cli(args, {suppress: suppress});
+cli( args );
