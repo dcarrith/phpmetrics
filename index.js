1db22ea9b4e4ee05a7f488e27ba8df6f5c5bebd6
Dave <dcarrith@gmail.com>
2016-01-26 23:12:59 -0500

    Committing the latest changes to index.js

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 var async = require('async');
 var execFile = require('child_process').execFile;
+var winston = require('winston');
 var phpCmd = 'php';
 var phpmetricsCmd = 'phpmetrics';
 
+winston.level = 'info';
+
+var logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.File)({filename: 'node.log'})
+    ]
+});
+
+if (process.env.NODE_ENV === 'test') {
+    winston.log('Removing winston.transports.Console as the logger.');
+    logger.remove(winston.transports.Console);
+} else {
+    winston.log('Adding winston.transports.Console as the logger.');
+    logger.add(winston.transports.Console, {prettyPrint: true});
+}
+
 function testPhp () {
   execFile(phpCmd, ['-v'], function (err, stdout, stderr) {
-    console.log(stdout)
     if (err) throw new Error(err)
   })
 }
@@ -16,19 +32,19 @@ function testPhpmetrics () {
   })
 }
 
-function phpmetrix( config, options, callback ) {
+function phpmetrix( args, options, callback ) {
 
-  result = execFile( phpmetricsCmd, [ '--config='+config ], {
+  result = execFile( phpmetricsCmd, args, {
     stdio: [
         0,      // Use parents stdin for child
         'pipe', // Pipe child's stdout to parent
-        null, // Pipe child's stderr to parent // fs.openSync('err.out', 'w') // Direct child's stderr to a file
+        null,   // Pipe child's stderr to parent // fs.openSync('err.out', 'w') // Direct child's stderr to a file
     ],
     cwd: process.cwd(),
     env: process.env
-  }, callback )
+  }, callback );
 
-  result.stdout.on( 'data', function( data ) {
+  /*result.stdout.on( 'data', function( data ) {
     // Data is binary at this point
     console.log( data );
   });
@@ -40,85 +56,62 @@ function phpmetrix( config, options, callback ) {
 
   result.on( 'close', function( code ) {
     // process has been closed
-  });
+    //winston.log( 'Process closed with code: ' + code);
+  });*/
 
   result.on( 'exit', function( code ) {
-    if( code !== 0 ) {
-        // process has exited
-        //console.log( 'Process exited with code: ' + code);
-    }
+    // process has exited
+    //winston.log( 'Process exited with code: ' + code);
   });
 
   return result;
 }
 
 module.exports = {
-  cli: function (cfg, opts, cb) {
 
-    var options = {
-      stdout: true,
-      stderr: true
-    }
-
-    var callback = function (error, stdout, stderr) {
-      //if (cb) return cb(error)
-      if (error) throw new Error(error)
-
-      /* To wait until the end of child_process.fileExec
-       * before outputting all stderr data:
-       */
-      /*if (stderr) {
-        if (cb) {
-            cb(stderr);
-        } else {
-            console.log(stderr);
-        }
-      }*/
-
-      /* To wait until the end of child_process.fileExec
-       * before outputting all stdout data:
-       */
-      /*if (stdout) {
-        if (cb) {
-            cb(stdout);
-        } else {
-            console.log(stdout);
-        }
-      }*/
-    }
-
-    return phpmetrix(cfg, options, callback)
+  cli: function( args, options, callback ) {
+    return phpmetrix( args, options, callback );
   },
 
-  phpmetrix: function (config, options, callback) {
+  phpmetrix: function( args, options, callback ) {
 
-    if (typeof options === 'function') {
-      callback = options
+    if ( typeof options === 'function' ) {
+      callback = options;
+
+      options = { stdio: [
+                        0,      // Use parents stdin for child
+                        'pipe', // Pipe child's stdout to parent
+                        null,   // Pipe child's stderr to parent // fs.openSync('err.out', 'w') // Direct child's stderr to a file
+                  ],
+                  quiet: false,
+                  debug: false };
     }
 
-    if (options.phpCmd) phpCmd = options.phpCmd
-    if (options.phpmetricsCmd) phpmetricsCmd = options.phpmetricsCmd
+    // Allow the path to php and phpmetrics be set by user
+    if ( options.phpCmd ) phpCmd = options.phpCmd;
+    if ( options.cmd ) phpmetricsCmd = options.cmd;
 
-    testPhp()
-    testPhpmetrics()
+    testPhp();
+    testPhpmetrics();
 
-    phpmetrix(config, options, callback)
+    phpmetrix( args, options, callback );
   },
 
-  gruntPlugin: function (grunt) {
-    grunt.task.registerMultiTask('phpmetrix', 'Run static-analysis on PHP files with phpmetrics.', function () {
+  gruntPlugin: function( grunt ) {
+    grunt.task.registerMultiTask( 'phpmetrix', 'Run static-analysis on PHP files with phpmetrics.', function() {
+
       var done = this.async()
 
       testPhp()
       testPhpmetrics()
 
-      // Merge task-specific and/or target-specific options with these defaults.
       var options = this.options({
-        stdout: true,
-        stderr: true
+        stdio: 'inherit',
+        quiet: false,
+        debug: false
       })
 
-      phpmetrix(config, options, done)
+      phpmetrix( args, options, done );
     })
   }
 }
